844ff25ff8bb214cf9ab23a0990ef5d424969fc3
Kudah Ndhlovu <eisaxoffice@gmail.com>
2026-02-25 14:48:16 +0200

    Add pubignore and drop build artifacts

--- rust/target/release/build/pixo-458411731ebb86ad/build_script_build-458411731ebb86ad.d
+++
@@ -1,5 +0,0 @@
-/Users/josphatndhlovu/Documents/WORK/Compiler/pixo_flutter/rust/target/release/build/pixo-458411731ebb86ad/build_script_build-458411731ebb86ad.d: build.rs
-
-/Users/josphatndhlovu/Documents/WORK/Compiler/pixo_flutter/rust/target/release/build/pixo-458411731ebb86ad/build_script_build-458411731ebb86ad: build.rs
-
-build.rs:
--- rust/target/release/deps/crossbeam_deque-b057276915cd287c.d
+++
@@ -1,8 +0,0 @@
-/Users/josphatndhlovu/Documents/WORK/Compiler/pixo_flutter/rust/target/release/deps/crossbeam_deque-b057276915cd287c.d: /Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/lib.rs /Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/deque.rs
-
-/Users/josphatndhlovu/Documents/WORK/Compiler/pixo_flutter/rust/target/release/deps/libcrossbeam_deque-b057276915cd287c.rlib: /Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/lib.rs /Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/deque.rs
-
-/Users/josphatndhlovu/Documents/WORK/Compiler/pixo_flutter/rust/target/release/deps/libcrossbeam_deque-b057276915cd287c.rmeta: /Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/lib.rs /Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/deque.rs
-
-/Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/lib.rs:
-/Users/josphatndhlovu/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.6/src/deque.rs:
